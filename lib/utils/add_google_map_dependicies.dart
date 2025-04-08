cde155afc74b2671295a76eaaab61eaaac03770e
RotRot-pi <ramyofficel@gmail.com>
2025-04-08 19:27:35 +0100

    Minor change

--- lib/utils/add_google_map_dependicies.dart
+++ lib/utils/add_google_map_dependicies.dart
@@ -43,28 +43,3 @@ Future<bool> addGoogleMapsDependency(String projectPath) async {
     return false;
   }
 }
-
-Future<bool> runFlutterPubGet(String projectPath) async {
-  log('Running "flutter pub get" in $projectPath...');
-  try {
-    final result = await Process.run(
-      'flutter',
-      ['pub', 'get'],
-      workingDirectory: projectPath,
-      runInShell: true,
-    );
-
-    if (result.exitCode == 0) {
-      log('"flutter pub get" completed successfully.');
-      log(result.stdout);
-      return true;
-    } else {
-      log('"flutter pub get" failed with exit code ${result.exitCode}');
-      log('Error Output:\n${result.stderr}');
-      return false;
-    }
-  } catch (e) {
-    log('Error running "flutter pub get": $e');
-    return false;
-  }
-}
